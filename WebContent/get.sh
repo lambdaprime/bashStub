a723e8706b46d9f98e075cc65edfbf2e64481965
lambdaprime <id.blackmesa@gmail.com>
2017-10-20 20:38:40 +0200

    Adding support for query params and headers

--- WebContent/get.sh
+++ WebContent/get.sh
@@ -1,5 +1,13 @@
 #
-# This script is called for GET requests.
+# This script is called for GET requests, in format:
+#
+# $ get.sh HEADERS PARAMS
+#
+# Where:
+#  
+# HEADERS - HTTP headers separated by newline
+# PARAMS - query parameters separated by newline
+#
 # It expects body of the request to be provided through stdin.
 # Everything which you send to stdout will be included in the body of the response.
 #
--- src/id/bashStub/BashStubServlet.java
+++ src/id/bashStub/BashStubServlet.java
@@ -25,12 +25,14 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Reader;
 import java.io.Writer;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 public class BashStubServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -51,8 +53,10 @@ public class BashStubServlet extends HttpServlet {
 
     void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String script = request.getMethod().toLowerCase() + ".sh";
+        
         Process proc = Runtime.getRuntime().exec(new String[] {"/bin/bash", 
-                getServletContext().getRealPath(script)});
+                getServletContext().getRealPath(script), headers(request), 
+                params(request.getParameterMap())});
         pipe(new InputStreamReader(request.getInputStream()), 
                 new OutputStreamWriter(proc.getOutputStream()));
         proc.getOutputStream().close();
@@ -60,6 +64,28 @@ public class BashStubServlet extends HttpServlet {
                 response.getWriter());
     }
 
+    private static String headers(HttpServletRequest request) {
+        StringBuilder buf = new StringBuilder();
+        Enumeration<?> names = request.getHeaderNames();
+        while (names.hasMoreElements()) {
+            String name = (String) names.nextElement();
+            buf.append(name + "=" + request.getHeader(name)).append('\n');
+        }
+        return buf.toString();
+    }
+    
+    private static String params(Map<String, ?> parameterMap) {
+        StringBuilder buf = new StringBuilder();
+        for (Object k: parameterMap.keySet()) {
+            String name = (String) k;
+            String val = "";
+            if (parameterMap.get(name) != null)
+                val = Arrays.asList((String[])parameterMap.get(name)).toString();
+            buf.append(name).append("=").append(val).append('\n');
+        }
+        return buf.toString();
+    }
+    
     private static void pipe(Reader reader, Writer writer) {
         char[] buf = new char[1024];
         try {
